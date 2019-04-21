c8843e4e12442fac09c22d4f21c1d343ee69648d
Mr QWC <alexnnian@protonmail.com>
2019-04-22 01:31:36 +0200

    Update Typo
    mnemonics -> Mnemonics

--- src/Gui/Common/MnemonicDialog.cpp
+++ src/Gui/Common/MnemonicDialog.cpp
@@ -16,7 +16,7 @@
 // along with Qwertycoin.  If not, see <http://www.gnu.org/licenses/>.
 
 #include "MnemonicDialog.h"
-#include "mnemonics/electrum-words.h"
+#include "Mnemonics/electrum-words.h"
 
 #include "ui_MnemonicDialog.h"
 
